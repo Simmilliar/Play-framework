1060c48c0cc0142ac9fee4569e48a539a10debbb
Simmilliar <simmilliar@gmail.com>
2017-08-28 12:55:11 +0300

    Added fetchUUID utils function. Made REGEX_UUID constant private.

--- app/controllers/utils/Utils.java
+++ app/controllers/utils/Utils.java
@@ -1,6 +1,8 @@
 package controllers.utils;
 
 import java.security.MessageDigest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utils
 {
@@ -9,8 +11,7 @@ public class Utils
 
 	private final String REGEX_NAME = "^[\\p{L}\\s'.-]+$";
 	private final String REGEX_EMAIL = "(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-	//todo it wants to be private too
-	public static final String REGEX_UUID = "([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})";
+	private final String REGEX_UUID = "([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})";
 
 	public boolean isNameValid(String name){
 		return name.matches(REGEX_NAME);
@@ -22,6 +23,11 @@ public class Utils
 
 	public boolean isUUIDValid(String uuid){
 		return uuid.matches(REGEX_UUID);
+	}
+
+	public String fetchUUID(String str) {
+		Matcher mailMatcher = Pattern.compile(REGEX_UUID).matcher(str);
+		return mailMatcher.find() ? mailMatcher.group(1) : null;
 	}
 
 	public static final String EMAIL_CONFIRMATION = "To complete your registration you need to confirm your e-mail address by following this link: %s";
--- test/controllers/RegistrationControllerFunctionalTest.java
+++ test/controllers/RegistrationControllerFunctionalTest.java
@@ -17,10 +17,7 @@ import play.test.WithApplication;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static controllers.utils.Utils.REGEX_UUID;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static play.inject.Bindings.bind;
@@ -84,11 +81,8 @@ public class RegistrationControllerFunctionalTest extends WithApplication {
 		verify(mockMailerService, times(1)).sendEmail(eq("valid@email.com"),
 				eq("Registration confirmation."), mailCaptor.capture());
 		assertTrue(mailCaptor.getValue().contains("To complete your registration you need to confirm your e-mail address by following this link:"));
-		Matcher mailMatcher = Pattern.compile(REGEX_UUID)
-				.matcher(mailCaptor.getValue());
-		assertTrue(mailMatcher.find());
-		Logger.debug(mailCaptor.getValue());
-		String confirmationKey = mailMatcher.group(1);
+		String confirmationKey = new Utils().fetchUUID(mailCaptor.getValue());
+		assertNotNull(confirmationKey);
 		assertEquals(new Utils().hashString(confirmationKey, confirmationKey), savedUser.getConfirmationKeyHash());
 		assertTrue(savedUser.getConfirmationKeyExpirationDate() > System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(59));
 	}
@@ -119,11 +113,8 @@ public class RegistrationControllerFunctionalTest extends WithApplication {
 		verify(mockMailerService, times(1)).sendEmail(eq("not-confirmed@email.com"),
 				eq("Registration confirmation."), mailCaptor.capture());
 		assertTrue(mailCaptor.getValue().contains("To complete your registration you need to confirm your e-mail address by following this link:"));
-		Matcher mailMatcher = Pattern.compile(REGEX_UUID)
-				.matcher(mailCaptor.getValue());
-		assertTrue(mailMatcher.find());
-		Logger.debug(mailCaptor.getValue());
-		String confirmationKey = mailMatcher.group(1);
+		String confirmationKey = new Utils().fetchUUID(mailCaptor.getValue());
+		assertNotNull(confirmationKey);
 		assertEquals(new Utils().hashString(confirmationKey, confirmationKey), savedUser.getConfirmationKeyHash());
 		assertTrue(savedUser.getConfirmationKeyExpirationDate() > System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(59));
 	}
